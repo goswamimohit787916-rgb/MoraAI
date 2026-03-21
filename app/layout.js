53473dd89ab7f9992b5cbb1758ec9b0a6056dc10
goswamimohit787916-rgb <goswamimohit787916@gmail.com>
2026-03-21 20:38:41 +0530

    Update and rename layout.tsx to layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,7 @@
 export default function RootLayout({ children }) {
   return (
     <html>
-      <body style={{ margin: 0, fontFamily: "Inter" }}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
